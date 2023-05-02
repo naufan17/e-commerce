b54ffab540bddef2a3818ff97509bc4d5a0ad971
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2023-05-02 11:12:12 +0700

    cofigure database MySQL

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	username string = "root"
+	password string = ""
+	database string = "ecommerce"
+)
+
+var (
+	dsn = fmt.Sprintf("%v:%v@/%v", username, password, database)
+)
+
+func MySQL() (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
